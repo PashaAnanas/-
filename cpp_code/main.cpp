4c1786d18dce0bfb9982021d17194b287a7f8720
F1XON <slobodyanzora@mail.ru>
2026-03-06 14:06:16 +0500

    main

---
+++ cpp_code/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "sosi" << std::flush;
+    std::cout << " " << std::flush;
+    std::cout << "xui" << std::endl;
+}
